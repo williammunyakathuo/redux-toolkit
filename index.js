c8d1f4b1661bb4b0af597f2198eaa5afda18bdde
williammunyakathuo <tjwellieyam@gmail.com>
2023-08-23 13:06:28 +0300

    logger middleware

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 
 console.log('Initial state ', store.getState())
 
-const unsubscribe = store.subscribe(()=>{
-    console.log('updated state', store.getState())
-})
+const unsubscribe = store.subscribe(()=>{})
 
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(3))
+
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(3))
 
 unsubscribe()
