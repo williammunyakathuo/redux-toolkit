57f0aea1551988081bb2e4f87ff874f0c1be1f22
williammunyakathuo <tjwellieyam@gmail.com>
2023-08-23 15:37:36 +0300

    extra reducers

--- features/icecream/icecreamSlice.js
+++ features/icecream/icecreamSlice.js
@@ -1,3 +1,5 @@
+const { cakeActions } = require('../cake/cakeSlice')
+
 const createSlice = require('@reduxjs/toolkit').createSlice
 
 
@@ -15,6 +17,11 @@ const icecreamSlice = createSlice({
         restocked : (state, action) =>{
             state.numOfIcecream += action.payload
         }
+    },
+    extraReducers: (builder) =>{
+        builder.addCase(cakeActions.ordered,( state) =>{
+            state.numOfIcecream--
+        })
     }
 })
 
